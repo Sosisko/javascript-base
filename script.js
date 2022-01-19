ff0907e45564d36e91017d7a014fcb5515c01198
Sosisko <svetlampy@mail.ru>
2022-01-19 21:28:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function aboba() {
+    let enterWord = prompt('Введите слово');
+    if (enterWord === Number) {
+        alert('Введи словами!')
+    }
+    else {
+        alert(enterWord);
+    }
+}
+
+aboba();
+
